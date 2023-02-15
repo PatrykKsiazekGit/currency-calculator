219e9de0829015d2771c8f18c61837567fa7055e
Patryk Ksiazek <krazekdnk@gmail.com>
2023-02-15 16:05:24 +0000

    calculator

---
+++ js/script.js
@@ -0,0 +1,44 @@
+{
+    const calculateResult = (amount, currency) => {
+        const rateUSD = 4.40;
+        const rateEUR = 4.68;
+        const rateGBP = 5.45;
+
+        switch (currency) {
+            case "GBP":
+                return amount / rateGBP;
+
+            case "USD":
+                return amount / rateUSD;
+
+            case "EUR":
+                return amount / rateEUR;
+        }
+    };
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.value = ` ${result.toFixed(2)} ${currency}`;
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+        updateResultText(amount, result, currency);
+
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
